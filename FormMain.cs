a031516eae2f6b136b7e7b463a383874a783ad74
BadRobotXIII <154725144+BadRobotXIII@users.noreply.github.com>
2024-07-05 16:00:47 -0500

    -Defaults json file -Read defaults from file -Write defaults file if nonexistent

--- FormMain.cs
+++ FormMain.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 using System.ComponentModel;
 using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
@@ -30,6 +32,67 @@ namespace EventMessageTool {
             this.progressBar = progressBar;
             this.tbEvents = tbEvents;
             this.tbDBName = tbDBName;
+        }
+
+        public class AppDefaults {
+            public string Module { get; set; }
+            public string DbName { get; set; }
+            public string IP { get; set; }
+            public string Tag { get; set; }
+            public string Path { get; set; }
+
+        }
+
+        private void SetAppDefaults() {
+            string appLocation = AppContext.BaseDirectory;
+            string appFileLoc = appLocation + "EventMsgDefaults.json";
+            bool defaultFileExists = File.Exists(appFileLoc);
+            AppDefaults defaults = new();
+            if (defaultFileExists != true) {
+                //Set form element defaults
+                defaults.Module = "1000";
+                defaults.DbName = "MyProject_db";
+                defaults.IP = "172.30.110.41";
+                defaults.Tag = "gMod2010_uaEventDetails";
+                defaults.Path = "C:\\user\\%USERPROFILE%\\documents";
+
+                tbModule.Text = defaults.Module;
+                tbDBName.Text = defaults.DbName;
+                tbIPAddress.Text = defaults.IP;
+                tbBaseTag.Text = defaults.Tag;
+                openFileDialog.InitialDirectory = defaults.Path;
+
+                //Format output
+                string jsonOut = JsonConvert.SerializeObject(defaults, Formatting.Indented);
+                //Write output file
+                File.WriteAllText(appLocation + "EventMsgDefaults.json", jsonOut);
+
+                //Print location of JSON file
+                EventMessage($"File {"EventMsgDefaults.json"} exported to {appLocation}");
+            } else {
+                var jsonIn = File.ReadAllText(appFileLoc);
+                var defaultsIn = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonIn);
+                if (defaultsIn.ContainsKey("Module")) {
+                    defaultsIn.TryGetValue("Module", out string value);
+                    tbModule.Text = value;
+                }
+                if (defaultsIn.ContainsKey("DbName")) {
+                    defaultsIn.TryGetValue("DbName", out string value);
+                    tbDBName.Text = value;
+                }
+                if (defaultsIn.ContainsKey("IP")) {
+                    defaultsIn.TryGetValue("IP", out string value);
+                    tbIPAddress.Text = value;
+                }
+                if (defaultsIn.ContainsKey("Tag")) {
+                    defaultsIn.TryGetValue("Tag", out string value);
+                    tbBaseTag.Text = value;
+                }
+                if (defaultsIn.ContainsKey("Path")) {
+                    defaultsIn.TryGetValue("Path", out string value);
+                    openFileDialog.InitialDirectory = value;
+                }
+            }
         }
 
         public FormMain() {
@@ -70,13 +133,14 @@ namespace EventMessageTool {
         }
 
         private void Form1_Load(object sender, System.EventArgs e) {
-            //Initialize defaults on form object load
+            SetAppDefaults();
             //AllocConsole(); //Display console for debug
-            tbDBName.Text = "MyProject_db";
-            tbModule.Text = "1000";
-            tbBaseTag.Text = "gMod2010_uaEventDetails";//Base tag
-            tbIPAddress.Text = "172.30.110.41";//Default IP Address
-            openFileDialog.InitialDirectory = "C:\\user\\%USERPROFILE%\\documents";//Default path
+            //Initialize defaults on form object load
+            //tbDBName.Text = "MyProject_db";
+            //tbModule.Text = "1000";
+            //tbBaseTag.Text = "gMod2010_uaEventDetails";//Base tag
+            //tbIPAddress.Text = "172.30.110.41";//Default IP Address
+            //openFileDialog.InitialDirectory = "C:\\user\\%USERPROFILE%\\documents";//Default path
         }
 
         private void btnFileSel_Click(object sender, EventArgs e) {
@@ -96,26 +160,36 @@ namespace EventMessageTool {
                 filePath = openFileDialog.FileName;
                 filePath = Environment.ExpandEnvironmentVariables(filePath);
             } else {
-                tbEvents.AppendText("Message: Invalid File\r\n");
+                EventMessage("Message: Invalid File\r\n");
             }
         }
 
         private void btnImportTags_Click(object sender, EventArgs e) {
-            //Start read thread
-            PLCComms plc = new(this);
-            var filePath = openFileDialog.FileName;
-            var threadImport = new Thread(() => tasks.ImportMessages(filePath, this));
-            threadImport.Start();
-
+            //Prompt user and confirm import
+            var promptResult = MessageBox.Show("Worksheet \"Fault Messages\" tab will be overwritten with PLC message contents.", "Proceed with upload?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (promptResult == DialogResult.Yes) {
+                //Start read thread
+                PLCComms plc = new(this);
+                var filePath = openFileDialog.FileName;
+                var threadImport = new Thread(() => tasks.ImportMessages(filePath, this));
+                threadImport.Start();
+            } else {
+                EventMessage("Message import aborted by user.");
+            }
         }
 
         private void btnExportMsg_Click(object sender, EventArgs e) {
-
-            //Create message export thread with anonymous function to pass arguments
-            var threadExportMsgs = new Thread(() => tasks.ExportMessages(filePath, this));
-            //Start write thread
-            if (threadExportMsgs.ThreadState != ThreadState.Running) {
-                threadExportMsgs.Start();
+            //Prompt user and confirm export
+            var promptResult = MessageBox.Show("PLC messages will be overwritten with Worksheet contents. ", "Proceed with download?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (promptResult == DialogResult.Yes) {
+                //Create message export thread with anonymous function to pass arguments
+                var threadExportMsgs = new Thread(() => tasks.ExportMessages(filePath, this));
+                //Start write thread
+                if (threadExportMsgs.ThreadState != ThreadState.Running) {
+                    threadExportMsgs.Start();
+                }
+            } else {
+                EventMessage("Message export aborted by user.");
             }
         }
 
@@ -150,7 +224,7 @@ namespace EventMessageTool {
         }
 
         private void toolTip1_Popup(object sender, PopupEventArgs e) {
-           
+
         }
     }
 }
